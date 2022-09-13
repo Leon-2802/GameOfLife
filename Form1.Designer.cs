5e5c9e8d7c7caaa8f8071d34f6d101eb01ae4f8f
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2022-09-13 12:35:11 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace GameOfLife
+{
+    partial class GameOfLife
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameArea = new System.Windows.Forms.PictureBox();
+            this.numCSize = new System.Windows.Forms.NumericUpDown();
+            this.cellSize = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.advanceBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gameArea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameArea
+            // 
+            this.gameArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameArea.BackColor = System.Drawing.Color.Black;
+            this.gameArea.Location = new System.Drawing.Point(12, 12);
+            this.gameArea.Name = "gameArea";
+            this.gameArea.Size = new System.Drawing.Size(920, 528);
+            this.gameArea.TabIndex = 0;
+            this.gameArea.TabStop = false;
+            this.gameArea.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gameArea_MouseClick);
+            // 
+            // numCSize
+            // 
+            this.numCSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numCSize.BackColor = System.Drawing.Color.LightSlateGray;
+            this.numCSize.ForeColor = System.Drawing.SystemColors.Window;
+            this.numCSize.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numCSize.Location = new System.Drawing.Point(71, 546);
+            this.numCSize.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numCSize.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numCSize.Name = "numCSize";
+            this.numCSize.ReadOnly = true;
+            this.numCSize.Size = new System.Drawing.Size(65, 23);
+            this.numCSize.TabIndex = 1;
+            this.numCSize.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // cellSize
+            // 
+            this.cellSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cellSize.AutoSize = true;
+            this.cellSize.BackColor = System.Drawing.Color.White;
+            this.cellSize.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cellSize.Location = new System.Drawing.Point(12, 548);
+            this.cellSize.Name = "cellSize";
+            this.cellSize.Size = new System.Drawing.Size(53, 15);
+            this.cellSize.TabIndex = 2;
+            this.cellSize.Text = "Cell Size:";
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.resetBtn.BackColor = System.Drawing.Color.LightSlateGray;
+            this.resetBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.resetBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.resetBtn.Location = new System.Drawing.Point(154, 546);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(70, 23);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = false;
+            this.resetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startBtn.BackColor = System.Drawing.Color.LightSlateGray;
+            this.startBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.startBtn.Location = new System.Drawing.Point(862, 548);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(70, 23);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // advanceBtn
+            // 
+            this.advanceBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.advanceBtn.BackColor = System.Drawing.Color.LightSlateGray;
+            this.advanceBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.advanceBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.advanceBtn.Location = new System.Drawing.Point(786, 548);
+            this.advanceBtn.Name = "advanceBtn";
+            this.advanceBtn.Size = new System.Drawing.Size(70, 23);
+            this.advanceBtn.TabIndex = 5;
+            this.advanceBtn.Text = "Advance";
+            this.advanceBtn.UseVisualStyleBackColor = false;
+            this.advanceBtn.Click += new System.EventHandler(this.AdvanceBtn_Click);
+            // 
+            // GameOfLife
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(944, 581);
+            this.Controls.Add(this.advanceBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.cellSize);
+            this.Controls.Add(this.numCSize);
+            this.Controls.Add(this.gameArea);
+            this.MinimumSize = new System.Drawing.Size(960, 620);
+            this.Name = "GameOfLife";
+            this.Text = "Game of Life";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameOfLife_FormClosing);
+            this.Load += new System.EventHandler(this.Load_GameOfLife);
+            ((System.ComponentModel.ISupportInitialize)(this.gameArea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox gameArea;
+        private System.Windows.Forms.NumericUpDown numCSize;
+        private System.Windows.Forms.Label cellSize;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button advanceBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class GameOfLife : Form
+    {
+        Boolean running = false;
+        Grid CellGrid;
+
+        public GameOfLife()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_GameOfLife(object sender, EventArgs e)
+        {
+            CreateGridSurface();
+        }
+
+        private void CreateGridSurface()
+        {
+            Point locPoint;
+            Cell newCell;
+            Random random = new Random();
+
+            int rows = (int)(gameArea.Height / numCSize.Value);
+            int cols = (int)(gameArea.Width / numCSize.Value);
+
+            //Create Grid Object
+            CellGrid = new Grid(rows, cols);
+
+
+            //using innnerhalb der Klammern limitiert die Existenz der Objekte auf diese Methode -> weniger Memory Müll
+            using (Bitmap bmp = new Bitmap(gameArea.Width, gameArea.Height))
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (SolidBrush cellBrush = new SolidBrush(Color.Green))
+            {
+                g.Clear(Color.Black);
+                gameArea.Image = (Bitmap)bmp.Clone();
+
+                Grid.gridCells.Clear();
+
+                for(int y = 0; y < rows; y++)
+                {
+                    for(int x = 0; x < cols; x++)
+                    {
+                        locPoint = new Point((int)(x * numCSize.Value), (int)(y * numCSize.Value));
+                        newCell = new Cell(locPoint, x, y);
+                        newCell.IsAlive = (random.Next(100) < 15) ? true : false; //true wenn kleiner als 15, false wenn größer
+                        Grid.gridCells.Add(newCell);
+                    }
+                }
+
+                Grid.gridCells = Grid.gridCells.OrderBy(c => c.XPos).OrderBy(c => c.YPos).ToList(); //nach y und x ordnen (wie im for loop vorgegeben)          
+
+                //Alle neuen Cells ins Grid laden:
+                foreach (Cell cell in Grid.gridCells)
+                {
+                    if(cell.IsAlive)
+                    {
+                        g.FillRectangle(cellBrush, new Rectangle(cell.Location, 
+                            new Size((int)numCSize.Value - 1, (int)numCSize.Value - 1)));
+                    }
+                }
+
+                gameArea.Image.Dispose(); //Befreit Memory von Ressourcen des vorherigen Bildes
+                gameArea.Image = (Bitmap)bmp.Clone();
+            }
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            CreateGridSurface();
+        }
+        private void AdvanceBtn_Click(object sender, EventArgs e)
+        {
+            GetNextState();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            running = !running;
+
+            startBtn.Text = running ? "Stop" : "Start";
+
+            while(running)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                GetNextState();
+                Application.DoEvents(); //Wait for operations to finish
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                if(elapsedMs < 101)
+                {
+                    int sleepTime = 100 - (int)elapsedMs;
+                    Thread.Sleep(sleepTime);
+                }
+            }
+        }
+
+        private void gameArea_MouseClick(object sender, MouseEventArgs e)
+        {
+            int cellIndex;
+
+            //Get cell size 
+            int cellSize = (int) numCSize.Value;
+
+            //Get the correct square on the grid, the mouse is hovering on
+            int Xloc = e.X / cellSize;
+            int YLoc = e.Y / cellSize;
+
+
+            //Get cell list index from grid index
+            cellIndex = (YLoc * CellGrid.Cols) + Xloc; // ! Formel für index
+
+            //Flip state of cell between dead and alive
+            Grid.gridCells[cellIndex].IsAlive = !Grid.gridCells[cellIndex].IsAlive;
+
+            UpdateGrid();
+        }
+
+        private void GameOfLife_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //End program when closing window
+            running = false;
+            Application.Exit();
+        }
+
+        private void GetNextState()
+        {
+            //Angrenzende Zellen prüfen auf IsAlive und Position -> Grid Updaten
+            /*
+            1. Weniger als zwei Nachbarn -> Tod
+            2. Zwei oder Drei Nachbarn -> bleibt am Leben
+            3. Mehr als drei Nachbarn -> Tod
+            4. Jede tote Zelle mit genau 3 Nachbarn wird wiederbelebt
+            */
+
+            foreach(Cell cell in Grid.gridCells)
+            {
+                int activeCount = CellGrid.LiveAdjacent(cell);
+
+                if (cell.IsAlive)
+                {
+                    if ((activeCount < 2) || (activeCount > 3))
+                        cell.AllowLiving = false;
+                    else
+                        cell.AllowLiving = true;
+                }
+                else
+                {
+                    if (activeCount == 3)
+                        cell.AllowLiving = true;
+                }
+            }
+
+            foreach(Cell cell in Grid.gridCells)
+            {
+                cell.IsAlive = cell.AllowLiving;
+            }
+
+            UpdateGrid();
+        }
+
+        private void UpdateGrid()
+        {
+            using (Bitmap bmp = new Bitmap(gameArea.Width, gameArea.Height))
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (SolidBrush cellBrush = new SolidBrush(Color.Green))
+            {
+                g.Clear(Color.Black);
+
+                foreach (Cell cell in Grid.gridCells)
+                {
+                    if (cell.IsAlive)
+                    {
+                        g.FillRectangle(cellBrush, new Rectangle(cell.Location,
+                            new Size((int)numCSize.Value - 1, (int)numCSize.Value - 1)));
+                    }
+                }
+
+                    
+                gameArea.Image.Dispose();
+                gameArea.Image = (Bitmap)bmp.Clone();
+            }
+        }
+
+    }
+
+    public class Grid
+    {
+        public static List<Cell> gridCells = new List<Cell>();
+
+        private int rows;
+        private int cols;
+
+        public Grid(int rows, int cols)
+        {
+            this.Rows = rows;
+            this.Cols = cols;
+        }
+
+        public int Rows
+        {
+           get { return rows; } 
+           set { rows = value; } 
+        }
+
+        public int Cols
+        {
+            get { return cols; }
+            set { cols = value; }
+        }
+
+        public int LiveAdjacent(Cell cell)
+        {
+            int liveAdjacent = 0;
+
+            int cellIndex = cell.YPos * this.Cols + cell.XPos; //Formel für index
+            int startIndex = cellIndex - this.Cols - 2;
+            int endIndex = cellIndex + this.Cols + 2;
+
+            //Verhindern, dass indezes über range des Grids gehen
+            startIndex = (startIndex < 0) ? 0 : startIndex;
+            endIndex = (endIndex > (Grid.gridCells.Count - 1)) ? Grid.gridCells.Count - 1 : endIndex;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (Math.Abs(cell.XPos - gridCells[i].XPos) < 2 && Math.Abs(cell.YPos - gridCells[i].YPos) < 2)
+                {
+                    if (Grid.gridCells[i].IsAlive && Grid.gridCells[i].Location != cell.Location)
+                        liveAdjacent++;
+                }
+            }
+
+            return liveAdjacent;
+        }
+    }
+
+    public class Cell
+    {
+        private Point location;
+        private int xPos;
+        private int yPos;
+        private Boolean isAlive;
+        private Boolean allowLiving;
+
+        public Cell(Point location, int x, int y)
+        {
+            this.Location = location;
+            this.XPos = x;
+            this.YPos = y;
+        }
+
+        public Point Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public int XPos
+        {
+            get { return xPos; }
+            set { xPos = value; }
+        }
+        public int YPos
+        {
+            get { return yPos; }
+            set { yPos = value; }
+        }
+        public Boolean IsAlive
+        {
+            get { return isAlive; }
+            set { isAlive = value; }
+        }
+        public Boolean AllowLiving
+        {
+            get { return allowLiving; }
+            set { allowLiving = value; }
+        }
+
+        public override string ToString()
+        {
+            //Override the way list is printed out:
+            return $"GridX:{this.XPos} GridY:{this.YPos} LocX{this.Location.X} LocY:{this.Location.Y}";
+        }
+    }
+}
